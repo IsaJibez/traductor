a25cefee827a471ed8bc18b85a62a0976ea85716
IAJB <isaiasjbaez4@gmail.com>
2023-05-12 20:58:09 -0400

    primer commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Traductor python
+
+Programa que introduces una palabra o frase y lo traduce a todos los idiomas registrado en la libreria [googletrans 3.1.0a0](https://py-googletrans.readthedocs.io/en/documentation/)
+
+Crea un archivo `frases.csv` que contiene toda las traducciones de la palabra/frase.
+
+Los idiomas con caracteres que no soporta el archivo csv lo imprime en pantalla.
+
+```python
+pip install googletrans==3.1.0a0
+```
---
+++ main.py
@@ -0,0 +1,25 @@
+import googletrans
+
+import io
+import csv
+
+traductor = googletrans.Translator()
+idiomas = googletrans.LANGUAGES
+texto = input()
+
+
+headers = ['idioma', 'texto']
+
+with open('traduccion.csv','w',newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow( headers)
+
+    for shortName,longName in idiomas.items():
+        traduccion = traductor.translate(text=texto,src='es',dest=shortName)    
+        idioma = [longName,traduccion.text]
+        
+        try:
+            writer.writerow(idioma)
+        except Exception as e:
+            print(f"{idioma[0]} : {idioma[1]}")
+print('listo')
